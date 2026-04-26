88f876d6a397395d24d4904801a21433d1b6976d
vaxo1971-cpu <vaxo1971@gmail.com>
2026-04-26 21:35:50 +0400

    fix bot

--- bot.py
+++ bot.py
@@ -1,23 +1,40 @@
+import requests
 import telebot
 import random
 import string
 
+API_URL = "https://last-spin-bot-1.onrender.com"
 TOKEN = "8250941489:AAG9wLq71wpbcwh3CTkS57DehPG8YIWdoZs"
 
 bot = telebot.TeleBot(TOKEN)
 
 def generate_code():
-    return "LS-" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    return "LS-" + ''.join(
+        random.choices(string.ascii_uppercase + string.digits, k=6)
+    )
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add("Play — 50 Stars")
-    bot.send_message(message.chat.id, "🎰 Welcome to Last Spin\nPress Play", reply_markup=markup)
+    bot.send_message(
+        message.chat.id,
+        "🎰 Welcome to Last Spin\nPress Play",
+        reply_markup=markup
+    )
 
 @bot.message_handler(func=lambda m: m.text == "Play — 50 Stars")
 def play(message):
     code = generate_code()
-    bot.send_message(message.chat.id, f"Your code:\n{code}")
+
+    requests.post(
+        f"{API_URL}/add",
+        json={"code": code}
+    )
+
+    bot.send_message(
+        message.chat.id,
+        f"Your code:\n{code}"
+    )
 
 bot.infinity_polling()
